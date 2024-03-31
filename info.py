fb2e0163ee0ac331abcdce78f22630794fc078e2
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2024-04-01 00:48:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID','22663326'))
+API_ID = int(environ['API_ID','22663326')
 API_HASH = environ.get('API_HASH','927e822ca6e854d8b7369c72ab9506e4')
 BOT_TOKEN = environ.get('BOT_TOKEN','7081840649:AAF0JJszTGMabkl6WscQbeX58UfgUluf4fk')
 
